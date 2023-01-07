fddb244451446b8984fb314e1c568999eeafcc32
Mohamad J. Makki <malanbaki@transforat.com>
2023-01-07 17:50:40 +0300

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
+import {RouterLink} from "@angular/router";
+import { HomeComponent } from './layout/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterLink,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
