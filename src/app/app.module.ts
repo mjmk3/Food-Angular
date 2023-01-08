5fff9d5ec799927acbb8b641fc5e8045c8a4f82c
Mohamad J. Makki <malanbaki@transforat.com>
2023-01-08 11:57:49 +0300

    updating the home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout/header/header.component';
 import {RouterLink} from "@angular/router";
-import { HomeComponent } from './layout/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './layout/header/header.component';
+import { HomeComponent } from './layout/home/home.component';
+import { RatingModule } from 'ng-starrating';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     RouterLink,
-    AppRoutingModule
+    AppRoutingModule,
+    RatingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Foods } from "../../share/model/food";
 
 @Injectable({
   providedIn: 'root'
@@ -6,16 +7,96 @@ import { Injectable } from '@angular/core';
 export class FoodService {
   constructor() { }
 
-  getAll():string[]{
+  getAll(): Foods[]{
     return [
-      '/assets/food-1.jpg',
-      '/assets/food-2.jpg',
-      '/assets/food-3.jpg',
-      '/assets/food-4.jpg',
-      '/assets/food-5.jpg',
-      '/assets/food-6.jpg',
-      '/assets/food-7.jpg',
-      '/assets/food-8.jpg'
-    ]
+      {
+        id: 1,
+        name: 'Pizza Pepperoni',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['Italy'],
+        star: 4.5,
+        imageUrl: 'assets/food-1.jpg',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      },
+      {
+        id: 2,
+        name: 'Meatball',
+        price: 20,
+        cookTime: '20-30',
+        favorite: true,
+        origins: ['Persia', 'Middle East', 'China'],
+        star: 4.7,
+        imageUrl: 'assets/food-2.jpg',
+        tags: ['SlowFood', 'Lunch'],
+      },
+      {
+        id: 3,
+        name: 'Hamburger',
+        price: 5,
+        cookTime: '10-15',
+        favorite: false,
+        origins: ['Germany', 'US'],
+        star: 3.5,
+        imageUrl: 'assets/food-3.jpg',
+        tags: ['FastFood', 'Hamburger'],
+      },
+      {
+        id: 4,
+        name: 'Fried Potatoes',
+        price: 2,
+        cookTime: '15-20',
+        favorite: true,
+        origins: ['Belgium', 'France'],
+        star: 3.3,
+        imageUrl: 'assets/food-4.jpg',
+        tags: ['FastFood', 'Fry'],
+      },
+      {
+        id: 5,
+        name: 'Chicken Soup',
+        price: 11,
+        cookTime: '40-50',
+        favorite: false,
+        origins: ['India', 'Asia'],
+        star: 3.0,
+        imageUrl: 'assets/food-5.jpg',
+        tags: ['SlowFood', 'Soup'],
+      },
+      {
+        id: 6,
+        name: 'Vegetables Pizza',
+        price: 9,
+        cookTime: '40-50',
+        favorite: false,
+        origins: ['Italy'],
+        star: 4.0,
+        imageUrl: 'assets/food-6.jpg',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      },
+      {
+        id: 7,
+        name: 'Spicy Cheese Burger',
+        price: 12,
+        cookTime: '20-30',
+        favorite: false,
+        origins: ['Indian'],
+        star: 4.0,
+        imageUrl: 'assets/food-7.jpg',
+        tags: ['FastFood', 'Burger', 'Lunch'],
+      },
+      {
+        id: 8,
+        name: 'Vegetables Magento Pizza',
+        price: 9,
+        cookTime: '45-50',
+        favorite: false,
+        origins: ['Indian'],
+        star: 4.0,
+        imageUrl: 'assets/food-8.jpg',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      }
+    ];
   }
 }
